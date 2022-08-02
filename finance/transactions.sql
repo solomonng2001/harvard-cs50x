78cbdfe00aab8049b517fca82fccd8cb105089fd
Solomon Ng <66112309+solomonng2001@users.noreply.github.com>
2022-08-02 12:46:34 +0800

    Delete finance directory

--- finance/transactions.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE transactions (id INTEGER, symbol TEXT NOT NULL, name TEXT NOT NULL, shares INTEGER NOT NULL, price NUMERIC NOT NULL, datetime_str TEXT NOT NULL, user_id INTEGER NOT NULL, PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES users(id));
